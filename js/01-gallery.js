58b853c2050a464b02eeadc1421f533153e5b647
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-04 23:15:13 +0100

    add gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+
+const renderGallery = (galleryItems) => {
+    return galleryItems.map(({preview, original, description}) => 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}"> 
+             <img class="gallery__image" 
+                src="${preview}"
+                data-source="${original}" 
+                alt="${description}"> 
+        </a>
+    </div>
+    `).join('');
+};
+
+galleryDiv.innerHTML = renderGallery(galleryItems);
+
+// const imageRef = document.querySelector('.gallery__image');
+
+
+
+const openModalOnclick = (event) => {
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img
+    src="${event.target.dataset.source}">
+    </div>
+`, {closable: true});
+
+    
+    instance.show();
+
+    if (basicLightbox.visible) {
+        const modalRef = document.querySelector('.modal');
+        modalRef.addEventListener('click', (event) => {
+            instance.close()}); 
+    }
+}
+
+galleryDiv.addEventListener('click', openModalOnclick);
+
+
+
