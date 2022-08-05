98e74ce2d671cb42c13f801f176a1c2c607228f1
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-05 14:54:57 +0100

    create lightbox gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,17 +7,20 @@ const galleryDiv = document.querySelector('.gallery');
 
 const renderGallery = (galleryItems) => {
     return galleryItems.map(({preview, original, description}) => 
-    `<div class="gallery__item">
-        <a class="gallery__link" href="${original}"> 
+    `<a class="gallery__item" href="${original}"> 
              <img class="gallery__image" 
                 src="${preview}"
-                data-source="${original}" 
                 alt="${description}"> 
-        </a>
-    </div>
+    </a>
     `).join('');
 };
 
 galleryDiv.innerHTML = renderGallery(galleryItems);
 
 
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
+
+
+
+// console.log(lightbox.captionData);
